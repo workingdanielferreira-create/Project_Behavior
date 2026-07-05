ef83431beb435ef9f16170107cb00c7907b8013f
workingdanielferreira-create <workingdanielferreira@gmail.com>
2026-07-05 19:22:53 +0200

    FX Creator: activation triggers, enemy_target anchor, basic_attack_radius, travel_forward/homing, Petals FX

--- tools/fx/main.js
+++ tools/fx/main.js
@@ -1,3 +1,4 @@
 // ---- [BOOT] wizard-only entry point ----
 window.setP=setP;window.bSetL=bSetL;window.chSet=chSet;window.sabSet=sabSet;window.wpnUndo=wpnUndo;window.wpnClear=wpnClear;window.renderStepUI=renderStepUI;window.renderWiz=renderWiz;window.wizNext=wizNext;window.wizPrev=wizPrev;window.gotoStep=gotoStep;window.openJson=openJson;
+window.trigToggle=trigToggle;window.trigSet=trigSet;window.trigCooldownSet=trigCooldownSet;window.statSet=statSet;
 rsz();bootWizard();requestAnimationFrame(tick);
